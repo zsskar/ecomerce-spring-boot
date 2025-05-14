94cc69e0347fe9fd23763577ad2a6b8f32c6bfba
Rashid Ali Khan <rashid.11612612@gmail.com>
2025-05-14 10:13:55 +0530

    Added others api for product

--- src/main/java/com/example/demo/product/controller/ProductController.java
+++ src/main/java/com/example/demo/product/controller/ProductController.java
@@ -19,4 +19,19 @@ public class ProductController {
         return ResponseEntity.ok(productService.getProductById(id));
     }
 
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        return ResponseEntity.ok(productService.createProduct(product));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product updatedProduct) {
+        return ResponseEntity.ok(productService.updateProduct(id, updatedProduct));
+    }
+
 }
