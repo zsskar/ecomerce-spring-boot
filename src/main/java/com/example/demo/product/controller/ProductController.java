e6452d3c34469862b0f2d4b2a7ba88fa1d7407b0
Rashid Ali Khan <rashid.ali.khan.-ND@disney.com>
2025-05-14 10:10:49 +0530

    Added others api for product

--- src/main/java/com/example/demo/product/controller/ProductController.java
+++ src/main/java/com/example/demo/product/controller/ProductController.java
@@ -1,17 +1,37 @@
 package com.example.demo.product.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.product.entity.Product;
+import com.example.demo.product.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
 
+    @Autowired
+    ProductService productService;
     @GetMapping("/{id}")
-    public String getProductById(@PathVariable Long id) {
-        return "Product details for ID: " + id;
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        return ResponseEntity.ok(productService.createProduct(product));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product updatedProduct) {
+        return ResponseEntity.ok(productService.updateProduct(id, updatedProduct));
     }
 
 }
