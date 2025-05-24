fb168ce7964d5517801a9efabed10766d8166067
Rashid Ali Khan <rashid.11612612@gmail.com>
2025-05-24 09:22:40 +0530

    some changes in product controller

--- src/main/java/com/example/demo/product/controller/ProductController.java
+++ src/main/java/com/example/demo/product/controller/ProductController.java
@@ -7,7 +7,6 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
